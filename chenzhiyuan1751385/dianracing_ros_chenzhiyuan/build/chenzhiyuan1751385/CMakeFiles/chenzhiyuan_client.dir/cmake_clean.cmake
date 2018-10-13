131e415f8eb6415386b33ea7601c6cc0355cacb4
MrC <786206514@qq.com>
2018-10-13 22:45:06 +0800

    change3

---
+++ chenzhiyuan1751385/dianracing_ros_chenzhiyuan/build/chenzhiyuan1751385/CMakeFiles/chenzhiyuan_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chenzhiyuan_client.dir/src/chenzhiyuan_client.cpp.o"
+  "/home/mrc/MyFile/homework/chenzhiyuan1751385/dianracing_ros_chenzhiyuan/devel/lib/chenzhiyuan1751385/chenzhiyuan_client.pdb"
+  "/home/mrc/MyFile/homework/chenzhiyuan1751385/dianracing_ros_chenzhiyuan/devel/lib/chenzhiyuan1751385/chenzhiyuan_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chenzhiyuan_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chenzhiyuan1751385/dianracing_ros_chenzhiyuan/build/chenzhiyuan1751385/CMakeFiles/chenzhiyuan_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chenzhiyuan_server.dir/src/chenzhiyuan_server.cpp.o"
+  "/home/mrc/MyFile/homework/chenzhiyuan1751385/dianracing_ros_chenzhiyuan/devel/lib/chenzhiyuan1751385/chenzhiyuan_server.pdb"
+  "/home/mrc/MyFile/homework/chenzhiyuan1751385/dianracing_ros_chenzhiyuan/devel/lib/chenzhiyuan1751385/chenzhiyuan_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chenzhiyuan_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
